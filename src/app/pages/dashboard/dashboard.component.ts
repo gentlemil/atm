91b12dd1f702d1e3c3ae4d3fd512f29b38ae1adf
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2025-02-07 00:54:44 +0100

    Add credit-card component, use that in dashboard view

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { CreditCardComponent } from '../../shared/credit-card/credit-card.component';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CreditCardComponent, NgIf, AsyncPipe, ButtonModule, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.sass',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  private authService = inject(AuthService);
+
+  public user$ = this.authService.userData();
+}
---
+++ src/app/shared/credit-card/credit-card.component.ts
@@ -0,0 +1,13 @@
+import { DatePipe, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { IUserAccount } from '../../core/models/types';
+
+@Component({
+  selector: 'app-credit-card',
+  imports: [NgIf, DatePipe],
+  templateUrl: './credit-card.component.html',
+  styleUrl: './credit-card.component.sass',
+})
+export class CreditCardComponent {
+  @Input({ required: true }) user!: IUserAccount;
+}
