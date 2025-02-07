34135e7d2ea9f1371cd656bde171bbd91bbab5a0
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2025-02-07 01:42:56 +0100

    Init vary basic balance view

--- src/app/pages/balance/balance.component.ts
+++ src/app/pages/balance/balance.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import { map } from 'rxjs';
+
+import { AuthService } from '../../core/services/auth.service';
+
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-balance',
   standalone: true,
-  imports: [],
+  imports: [NgIf, AsyncPipe, ButtonModule, RouterLink],
   templateUrl: './balance.component.html',
   styleUrl: './balance.component.sass',
 })
-export class BalanceComponent {}
+export class BalanceComponent {
+  private authService = inject(AuthService);
+
+  public userBalance$ = this.authService
+    .userData()
+    .pipe(map((res) => res.account_balance));
+}
