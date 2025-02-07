656291948f1a52670169ed30c0a12e98e4d5a83c
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2025-02-07 01:31:43 +0100

    Add error handling in auth-interceptor

--- src/app/core/interceptors/authInterceptor.ts
+++ src/app/core/interceptors/authInterceptor.ts
@@ -1,10 +1,13 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 
 import { PersistanceService } from '../services/persistance.service';
+import { catchError, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 export const authInterceptor: HttpInterceptorFn = (request, next) => {
   const persistanceService = inject(PersistanceService);
+  const toast = inject(ToastrService);
 
   if (request.headers.has('X-Skip-Interceptor')) {
     const headers = request.headers.delete('X-Skip-Interceptor');
@@ -19,5 +22,16 @@ export const authInterceptor: HttpInterceptorFn = (request, next) => {
     },
   });
 
-  return next(request);
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      let errorMessage = 'An unknown error occurred!';
+      if (error.error instanceof ErrorEvent) {
+        errorMessage = `Error: ${error.error.message}`; // Client-side error
+      } else {
+        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`; // Server-side error
+      }
+      toast.error(errorMessage);
+      return throwError(() => new Error(errorMessage));
+    })
+  );
 };
